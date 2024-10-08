326fe238a77de520bfea3f1ac27ca0cead2f7add
v-sekulic <v.sekulic@vegait.rs>
2024-10-08 14:32:02 +0200

    removes app.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import App from './App.tsx';
+
 import '../../../packages/ui-kit/src/styles/index.css';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 
